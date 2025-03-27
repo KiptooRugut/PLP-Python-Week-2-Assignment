912dd8943c04276416e6e8de671eeef3a63a44b3
KiptooRugut <mccrug97@gmail.com>
2025-03-27 16:57:37 +0300

    Append elements 10, 20, 30, 40 to my_list.

--- Week2-Assignment.py
+++ Week2-Assignment.py
@@ -1,2 +1,8 @@
 # Step 1: Create an empty list called my_list
 my_list = []
+
+# Step 2: Append elements 10, 20, 30, 40 to my_list.
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
